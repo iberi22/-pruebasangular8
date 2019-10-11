04fe5e561ece4f15f1030762a2e93e464517fbc1
BeRi <iberi22@gmail.com>
2019-10-10 23:19:15 -0500

    style and routing done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,4 +13,11 @@ export class HomeComponent implements OnInit {
     countClick() {
         this.clickCounter += 1;
     }
+   setClasses(){
+       let myClasses ={
+           active: this.clickCounter > 2,
+           noactive :this.clickCounter < 2,
+       }
+       return myClasses;
+   }
 }
